5112f521230b1a7a5cbebc465fc80f125cdbde20
IvanMusila <imusila714@gmail.com>
2025-06-03 07:44:47 +0300

    build and deploy changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Set to '/' for root domain or '/subpath/' if needed
+  build: {
+    outDir: 'dist', // Ensure this matches Netlify's publish dir
+    assetsDir: 'assets', // Default (where JS/CSS are stored)
+  }
+});
